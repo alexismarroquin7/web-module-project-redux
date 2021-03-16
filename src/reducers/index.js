f5cbadb860221b8e29255b35df13f3491ad0c3f7
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-03-16 12:22:20 -0700

    reducer and addFeature action creator initial setup

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+    myState: "its working"
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+};
